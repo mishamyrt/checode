75f4694867010c770f68eb725be9273d1f823c09
mishamyrt <misha@myrt.co>
2020-12-24 23:22:38 +0300

    substring: Add helpers tests

---
+++ pkg/substring/helpers_test.go
@@ -0,0 +1,48 @@
+package substring_test
+
+import (
+	"testing"
+
+	"github.com/mishamyrt/checode/v1/pkg/substring"
+)
+
+type Trimmer func(text string) string
+
+type Subsequenter func(delimeter string, text string) string
+type Previouser func(delimeter string, text string) string
+
+func TrimSuite(t *testing.T, trim Trimmer) {
+	res := trim(" * Test 	")
+	if res != "Test" {
+		t.Errorf("Wrong trim: \"%s\"", res)
+		t.Fail()
+	}
+}
+
+func SubsequentSuite(t *testing.T, sub Subsequenter) {
+	res := sub(":", "Junk :Test")
+	if res != "Test" {
+		t.Errorf("Wrong subsequent: \"%s\"", res)
+		t.Fail()
+	}
+}
+
+func PreviousSuite(t *testing.T, prev Previouser) {
+	res := prev(":", "Test: Junk")
+	if res != "Test" {
+		t.Errorf("Wrong previous: \"%s\"", res)
+		t.Fail()
+	}
+}
+
+func TestTrim(t *testing.T) {
+	TrimSuite(t, substring.Trim)
+}
+
+func TestSubsequent(t *testing.T) {
+	SubsequentSuite(t, substring.GetSubsequent)
+}
+
+func TestPrevious(t *testing.T) {
+	PreviousSuite(t, substring.GetPrevious)
+}
